26e756f89f8c3fc87cdc823eb74eca903156c06b
Dglaser <glaser.des@gmail.com>
2017-10-12 21:21:36 -0700

    Update ft_placement.c
    fixed some errors

--- ft_placement.c
+++ ft_placement.c
@@ -13,7 +13,7 @@ int		ft_checkempty(int i, int *smallest_tetrissq, int *figure)
 
 	x = 0;
 	if((smallest_tetrissq[i + figure[1]] == '.') && (smallest_tetrissq[i + figure[2]] == '.')
-		   	&& (smallest_tetrissq[i + figure[3i]] == '.'))
+		   	&& (smallest_tetrissq[i + figure[3]] == '.'))
 		return (1);
 	return (0);
 }
@@ -21,14 +21,14 @@ int		ft_checkempty(int i, int *smallest_tetrissq, int *figure)
 int		ft_placement(int smallest_tetrissq, int *figure, int n)
 {
 	int x;
-	int o;
+	int i;
 	int dist;
 
 	x = 0;
-	o = 0;
+	i = 0;
 	while(x <= n)
 	{
-		while(smallest_tetrissq != '\0')
+		while(smallest_tetrissq[i] != '\0')
 		{
 			if(smallest_tetrissq[i] == '.')
 			{
