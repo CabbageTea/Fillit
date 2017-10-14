d335d34ecb01c7edf6ffc7442a21b5d6e2175b3f
Dagoberto Montoya <dmontoya@e1z2r9p4.42.us.org>
2017-10-14 16:01:10 -0700

    findbesttetris.c

--- findbesttetris.c
+++ findbesttetris.c
@@ -6,32 +6,34 @@
 /*   By: dmontoya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/10 20:59:19 by dmontoya          #+#    #+#             */
-/*   Updated: 2017/10/10 20:59:20 by dmontoya         ###   ########.fr       */
+/*   Updated: 2017/10/14 15:59:26 by dmontoya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "tetris.h"
 
-char    *squaresize(int requiredsize)
+char	*squaresize(int requiredsize)
 {
-    char *sq;
-    int sqsize;
-    int newlinespots;
+	char	*sq;
+	int		sqsize;
+	int		newlines;
 
-    newlinespots = requiredsize + 1;
-    sqsize = (requiredsize ^ 2) + requiredsize + 1; //^2 the rows of characters, + requiredsize is the sum of the '\n' characters, + 1 for the ending '\0'
-    sq = ft_memalloc(requiredsize);
-    ft_memset(sq, '.', sqsize - 1);
-    while (sq[newlinespots] != '\0')
-    {
-        sq[newlinespots] = '\n';
-        newlinespots = newlinespots * 2;
-        if (newlinespots > sqsize)
-            break ;
-    }
-    return (sq);
+	newlines = requiredsize;
+	sqsize = (requiredsize ^ 2) + newlines + 1; //^2 the rows of characters, + requiredsize is the sum of the '\n' characters, + 1 for the ending '\0'
+	sq = ft_memalloc(sqsize);
+	ft_memset(sq, '.', sqsize - 1);
+	while (sq[newlinespots] != '\0')
+	{
+		sq[newlines] = '\n';
+		newlines = newlines + (requiredsize + 1) ;
+		if (newlines > sqsize)
+			break ;
+	}
+	sq[sqsize - 1] = '\0';
+	return (sq);
 }
+
 void    empty_string(char *small_tetris_sq)
 {
     int i;
@@ -44,27 +46,34 @@ void    empty_string(char *small_tetris_sq)
         i++;
     }
 }
-int     fillit(char *small_tetris_sq, int *tetconf,)
+int     fillit(char *small_tetris_sq, int *tetconf, int size, int tetcount, int y)
 {
     int ipos;
+	int i;
 
+	i = 0;
     ipos = ft_placement(small_tetris_sq, tetconf, size);
-    if (ipos == -1)
-        return (ipos);
+	if (ipos == -1)
+		return (-1);
+	while (i < 4)
+		small_tetris_sq[ipos + tetconf[i++]] = y + 65;
+	return (1);
 }
 
 int    recursive_backtrack(char *small_tetris_sq, int **tetconf, int size, int tetcount)
 {
     int y;
-    
-    while (tetconf[y] < tetcount)
+
+	y = 0;
+    while (y < tetcount)
     {
-        if (fillit(small_tetris_sq, tetconf[y], size) != -1))
+        if (fillit(small_tetris_sq, tetconf[y], size, tetcount, y) != -1))
         {
             size = squaresize(size + 1);
             empty_string(small_tetris_sq);
+			y = 0;
         }
-        return (recursive_backtrack(small_tetris_sq, tetconf, --configs, size));
+		y++;
     }
     return (1);
 }
